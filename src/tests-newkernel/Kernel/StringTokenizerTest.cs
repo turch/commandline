1d9e8569e821249d21c2843ede020383fb289103
gsscoder <gsscoder@gmail.com>
2013-03-08 16:13:26 +0100

    Adding new unit test project specific for new kernel (ongoing desing).

---
+++ src/tests-newkernel/Kernel/StringTokenizerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine.Kernel;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+
+namespace CommandLineUnitTest.Kernel
+{
+    public class StringTokenizerTest
+    {
+        [Fact]
+        public void SutIsStringTokenizer()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new StringTokenizer();
+            // Verify outcome
+            Assert.IsAssignableFrom<IStringTokenizer>(sut);
+            // Teardown
+        }
+    }
+}
