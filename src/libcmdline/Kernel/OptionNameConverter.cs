5aa11ae4d8c8ea506099d1eb06b31c9828859c36
gsscoder <gsscoder@gmail.com>
2013-03-10 07:40:03 +0100

    ArgumentTokenizer.

--- src/libcmdline/Kernel/OptionNameConverter.cs
+++ src/libcmdline/Kernel/OptionNameConverter.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CommandLine.Kernel
+﻿namespace CommandLine.Kernel
 {
     /// <summary>
     /// Contains extension methods to convert option names.
