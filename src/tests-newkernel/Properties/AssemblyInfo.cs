0d9788c8740e797248e8f05ebea83acbbcce0d13
gsscoder <gsscoder@gmail.com>
2013-03-11 11:13:48 +0100

    Removing newkernel test assembly (refactoring will continue on existing one).

--- src/tests-newkernel/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("CommandLineUnitTest.dll")]
-[assembly: AssemblyDescription("Command Line Parser Library allows CLR applications to define a syntax for parsing command line arguments.")]
-[assembly: AssemblyCulture("")]
