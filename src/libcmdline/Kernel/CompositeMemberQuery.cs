8cbba7c747651526d62a2f221d642299c3bc7dd9
gsscoder <gsscoder@gmail.com>
2013-03-12 14:06:57 +0100

    Outlining CompositeMemberQuery to treat different IMember as whole.

---
+++ src/libcmdline/Kernel/CompositeMemberQuery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandLine.Kernel
+{
+    internal class CompositeMemberQuery : IMemberQuery
+    {
+        private readonly IEnumerable<IMemberQuery> _queries;
+
+        public CompositeMemberQuery(IEnumerable<IMemberQuery> queries)
+            : this(queries.ToArray())
+        {
+        }
+
+        public CompositeMemberQuery(params IMemberQuery[] queries)
+        {
+            if (queries == null)
+            {
+                throw new ArgumentNullException("queries");
+            }
+
+            _queries = queries;
+        }
+
+        public IEnumerable<IMember> SelectMembers(Type type)
+        {
+            return (from query in _queries
+                    from result in query.SelectMembers(type)
+                    select result);
+        }
+    }
+}
